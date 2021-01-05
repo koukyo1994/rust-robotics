dc810e374779c7a3ee2a92a19d8b260371963262
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-01-05 22:41:22 +0900

    Add pipeline

--- LNPR_BOOK/lidar_200/src/main.rs
+++ LNPR_BOOK/lidar_200/src/main.rs
@@ -1,24 +1,40 @@
 use polars::prelude::*;
 use std::fs::File;
 
-fn load_txt() -> DataFrame {
+fn read_txt() -> Result<DataFrame> {
     let file = File::open("../sensor_data/sensor_data_200.txt").expect("could not read file");
-
-    let mut df = CsvReader::new(file)
+    CsvReader::new(file)
         .infer_schema(None)
         .has_header(false)
         .with_delimiter(b' ')
         .finish()
-        .unwrap();
+}
 
-    df.rename("column_1", "date").ok();
-    df.rename("column_2", "time").ok();
-    df.rename("column_3", "ir").ok();
-    df.rename("column_4", "lidar").ok();
+fn rename_cols(mut df: DataFrame) -> Result<DataFrame> {
+    (1..5)
+        .zip(&["date", "time", "ir", "lidar"])
+        .for_each(|(idx, name)| {
+            df.rename(format!("{}{}", "column_", idx).as_str(), name)
+                .ok();
+        });
 
-    df
+    Ok(df)
+}
+
+fn print_state(df: DataFrame) -> Result<DataFrame> {
+    println!("{:?}", df.head(Some(3)));
+    Ok(df)
+}
+
+fn pipeline() -> Result<DataFrame> {
+    read_txt()?
+        .pipe(print_state)
+        .unwrap()
+        .pipe(rename_cols)
+        .expect("could not rename columns")
+        .pipe(print_state)
 }
 
 fn main() {
-    println!("{}", load_txt());
+    let _df = pipeline().expect("could not prepare DataFrame");
 }
