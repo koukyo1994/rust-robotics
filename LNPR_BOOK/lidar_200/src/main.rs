d7ad711d4ed640f73aad39c815faa35a4096ff45
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-01-05 23:26:52 +0900

    WIP histogram for lidar_200

--- LNPR_BOOK/lidar_200/src/main.rs
+++ LNPR_BOOK/lidar_200/src/main.rs
@@ -1,3 +1,4 @@
+use plotters::prelude::*;
 use polars::prelude::*;
 use std::fs::File;
 
@@ -35,6 +36,44 @@ fn pipeline() -> Result<DataFrame> {
         .pipe(print_state)
 }
 
+fn draw_histogram(df: DataFrame) -> Result<DataFrame> {
+    let root = BitMapBackend::new("lidar_200_histogram.png", (640, 480)).into_drawing_area();
+
+    root.fill(&WHITE).expect("Failed to fill histogram");
+
+    let lidar = df.column("lidar").expect("could not find `lidar` column");
+    let lidar_max = lidar.max().expect("cannot take max operation") as u32;
+    let lidar_min = lidar.min().expect("cannot take min operation") as u32;
+
+    let mut chart = ChartBuilder::on(&root)
+        .x_label_area_size(35)
+        .y_label_area_size(40)
+        .margin(5)
+        .caption("Lidar Histogram", ("sans-serif", 30))
+        .build_cartesian_2d((lidar_min..lidar_max).into_segmented(), 0u32..5000u32)
+        .expect("could not prepare chart");
+
+    chart
+        .configure_mesh()
+        .bold_line_style(&WHITE.mix(0.3))
+        .y_desc("Count")
+        .x_desc("Lidar")
+        .axis_desc_style(("sans-serif", 15))
+        .draw()
+        .expect("could not draw chart");
+
+    chart
+        .draw_series(
+            Histogram::vertical(&chart)
+                .style(BLUE.mix(0.5).filled())
+                .data(lidar.as_ref().into_iter().map(|x: &i64| (*x, 1))),
+        )
+        .expect("could not draw series");
+
+    Ok(df)
+}
+
 fn main() {
-    let _df = pipeline().expect("could not prepare DataFrame");
+    let df = pipeline().expect("could not prepare DataFrame");
+    draw_histogram(df);
 }
